f966b63bc48864a472defdff350a5e10d1075b07
Bhuwan-Shahi <shahibhuwan265@gmail.com>
2024-10-18 19:31:50 +0545

    js

---
+++ js/Odin/script.js
@@ -0,0 +1,97 @@
+// // // const btn = document.querySelector("button")
+
+// // // const username = prompt("Enter you name:")
+
+// // // if(username == "bhuwan"){
+// // //     btn.addEventListener("onclick", console.log("hello"+username));
+// // // }else{
+// // //     btn.addEventListener("on")
+// // // }
+
+
+// const btn = document.querySelector(".helo");
+// btn.addEventListener('click', () => {
+//   alert("hokage dattebayo");
+// })
+
+
+
+
+
+
+
+// function test(arg ) {
+//   arg()
+// }
+
+// function best() {
+//   console.log("khjsfkjsdhfhdf")
+// }
+
+
+
+// test(best);
+
+
+
+
+
+
+
+// // //multi line arrow function
+
+// // // let sum = (a,b)=>{
+// // //     let result = a+b;
+// // //     return result;
+// // // };
+
+// // // alert(sum(3,600));
+
+
+// // //ARROW FUNCTON FOR THE REUGAL USE 
+// // let username = (a) => {
+// //     let k = a + " is that you name?";
+// //     return k;
+// // } 
+
+
+// // alert(username("ram"));
+
+// //  FUNCTION EXERCISES
+// // function aadd(){
+// //     let a = prompt("Enter a number:");
+// //     return a + 7;
+// // }
+
+// // alert (aadd());
+
+
+// // function mul(k, kk){
+// //     let sum = k * kk;
+// //     return sum;
+// // }
+// // alert(mul(56 , 1))
+
+// //FUNCTION THAT MAKE THE FIRST LEETER OF STIRNG CAPITAL
+
+// function capitalize(str) {
+//     return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase;function capatililze(bhuwan){
+       
+//   }
+// }
+
+  const cars =["red", "hat", "bone"];
+  // cars.push(6);
+  // cars.pop()
+
+  //ADDING IN THE FIRST INDEX AT ARRAY
+  cars.unshift("hello");
+  cars.shift();
+
+  console.log(cars);
+  console.log(cars.includes(34))
+  console.log(cars.indexOf("red"))
+
+  // new aray
+  const newArr = cars.join()
+  console.log(newArr);
