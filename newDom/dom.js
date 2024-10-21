c5aab4654f751130236e336e2455944b3385ca9c
Bhuwan-Shahi <shahibhuwan265@gmail.com>
2024-10-21 13:26:21 +0545

    dom

--- newDom/dom.js
+++ newDom/dom.js
@@ -5,3 +5,20 @@ const title = document.querySelector('#Main-heading')
 title.style.color = 'red'
 console.log(title)
 
+
+//create elements
+const ul = document.querySelector('ul')
+//adding elements
+const li = document.createElement('li')
+
+
+//modify text contnent
+const firstListItem = document.querySelector('Main-heading')
+console.log(firstListItem.textContent)
+console.log(firstListItem.innerHTML)
+console.log(firstListItem.innerText)
+
+
+
+ul.append(li)
+
